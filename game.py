c4e6e2892a4bbf02948cd2524ec91c53fe0b4e9a
GulsahYagci34 <39766848+GulsahYagci34@users.noreply.github.com>
2020-07-26 15:37:53 +0300

    Kenarlara değince oyunun bitmesi

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
-#Kütüphane
+#Kütüphaneler
 import pygame
+import time
 
 
 #Screen yaratma
@@ -9,17 +10,32 @@ white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 
+display_width = 800
+display_height = 600
 
-display = pygame.display.set_mode((800,600)) # screen size 400x300
+
+
+display = pygame.display.set_mode((display_width,display_height)) # screen size
 pygame.display.set_caption('Snake')
 
-x1 = 300
-y1 = 300
+x1 = display_width/2
+y1 = display_height/2
+
+snake_block = 10
 
 x1_change = 0
 y1_change = 0
 
 clock = pygame.time.Clock()
+snake_speed = 30
+
+
+font_style = pygame.font.SysFont(None,50)
+
+def message(msg,color):
+    mesg = font_style.render(msg, True, color)
+    display.blit(mesg, [display_width/2, display_height/2])
+
 
 
 game_over = False # oyunun bitip bitmediğinin kontrolu
@@ -42,14 +58,22 @@ while not game_over:
                 x1_change = 0
                 y1_change = 10
 
+    if x1 >= display_width or x1< 0 or y1 >= display_height:
+        game_over=True
+
     x1 += x1_change
     y1 += y1_change
 
     display.fill(white)
-    pygame.draw.rect(display,black,[x1,y1,10,10])
+    pygame.draw.rect(display,black,[x1,y1,snake_block,snake_block])
 
     pygame.display.update()
-    clock.tick(30)
+    clock.tick(snake_speed)
+
+
+message('Game Over',red)
+pygame.display.update()
+time.sleep(2)
 
 pygame.quit()
 quit()
