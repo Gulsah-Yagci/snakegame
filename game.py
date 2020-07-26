d3a1d3cb00abc09c513221182cc7cbe63cca0b7a
GulsahYagci34 <39766848+GulsahYagci34@users.noreply.github.com>
2020-07-26 17:20:15 +0300

    Yılanın yemi yedikten sonra uzaması

--- game.py
+++ game.py
@@ -7,13 +7,15 @@ pygame.init()
 
 # renkler
 white = (255, 255, 255)
+yellow = (255,255,102)
 black = (0, 0, 0)
 red = (255, 0, 0)
+green = (0,255,0)
 blue = (0, 0, 255)
 
 # ekran boyutu
-display_width = 800
-display_height = 600
+display_width = 600
+display_height = 400
 
 # ekran boyunu set etme ve isimlerdirme
 display = pygame.display.set_mode((display_width, display_height))
@@ -22,9 +24,14 @@ pygame.display.set_caption('Snake')
 clock = pygame.time.Clock()
 
 snake_block = 10
-snake_speed = 30
+snake_speed = 15
 
 font_style = pygame.font.SysFont(None, 30)
+score_font = pygame.font.SysFont(None, 30)
+
+def snake(snake_block,snake_list):
+    for i in snake_list:
+        pygame.draw.rect(display,black,[i[0],i[1],snake_block,snake_block])
 
 # message fonksiyonu
 def message(msg, color):
@@ -42,14 +49,18 @@ def gameLoop():
     x1_change = 0
     y1_change = 0
 
+    # yılanın uzunluğu ve uzunluğunun kontrol edildiği liste değişkeni
+    snake_list = []
+    len_of_snake = 1
+
     foodx = round(random.randrange(0, display_width - snake_block) / 10.0) * 10.0
     foody = round(random.randrange(0, display_width - snake_block) / 10.0) * 10.0
 
     while not game_over:
 
         while game_close == True:
-            display.fill(white)
-            message("Game Over! Press Q-Quit or C-Play Again", red)
+            display.fill(blue)
+            message("Game Over! \n Press Q-Quit or C-Play Again", red)
             pygame.display.update()
 
             for event in pygame.event.get():
@@ -82,13 +93,27 @@ def gameLoop():
 
         x1 += x1_change
         y1 += y1_change
-        display.fill(white)
-        pygame.draw.rect(display, blue, [foodx, foody, snake_block, snake_block])
-        pygame.draw.rect(display, black, [x1, y1, snake_block, snake_block])
+        display.fill(blue)
+        pygame.draw.rect(display, green, [foodx, foody, snake_block, snake_block])
+        snake_head = []
+        snake_head.append(x1)
+        snake_head.append(y1)
+        snake_list.append(snake_head)
+
+        if len(snake_list) > len_of_snake:
+            del snake_list[0]
+
+        for i in snake_list[:-1]:
+            if i == snake_head:
+                game_close = True
+        snake(snake_block, snake_list)
+
         pygame.display.update()
 
         if x1 == foodx and y1 == foody:
-            print("Got it!")
+            foodx = round(random.randrange(0, display_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, display_height - snake_block) / 10.0) * 10.0
+            len_of_snake += 1
         clock.tick(snake_speed)
 
     pygame.quit()
