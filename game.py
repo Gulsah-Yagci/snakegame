a1f1e023632bb854e488ae7eec6166f98c74aacf
GulsahYagci34 <39766848+GulsahYagci34@users.noreply.github.com>
2020-07-26 15:23:44 +0300

    yılını hareket ettirme

--- game.py
+++ game.py
@@ -3,14 +3,23 @@ import pygame
 
 
 #Screen yaratma
-
 pygame.init()
-display = pygame.display.set_mode((400,300)) # screen size 400x300
-pygame.display.update() # animasyonlarda hareketi sağlamak için sürekli frame update edilir
+
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+
+
+display = pygame.display.set_mode((800,600)) # screen size 400x300
 pygame.display.set_caption('Snake')
 
-blue = (0,0,255)
-red = (255,0,0)
+x1 = 300
+y1 = 300
+
+x1_change = 0
+y1_change = 0
+
+clock = pygame.time.Clock()
 
 
 game_over = False # oyunun bitip bitmediğinin kontrolu
@@ -19,9 +28,28 @@ while not game_over:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game_over=True
-    # yılanı çizdirme
-    pygame.draw.rect(display,blue,[200,150,10,10])
+        if event.type ==pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10
+                y1_change = 0
+            if event.key == pygame.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            if event.key == pygame.K_UP:
+                x1_change = 0
+                y1_change = -10
+            if event.key == pygame.K_DOWN:
+                x1_change = 0
+                y1_change = 10
+
+    x1 += x1_change
+    y1 += y1_change
+
+    display.fill(white)
+    pygame.draw.rect(display,black,[x1,y1,10,10])
+
     pygame.display.update()
+    clock.tick(30)
 
 pygame.quit()
 quit()
